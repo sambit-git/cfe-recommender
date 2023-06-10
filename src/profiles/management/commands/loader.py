46209859d561c1a8e9a6915c59698c5198487b15
Sambit Kumar <sambit.kumar142@gmail.com>
2023-06-10 20:28:59 +0530

    Load movie data through CLI
    - updated loader to load movie data as well
    - modified existing arguments
    Now to load users use the below command
        - python manage.py loader --users <int> --show-total
    And to load movies use the below commad
        - python manage.py loader --movies <int> --show-total

--- src/profiles/management/commands/loader.py
+++ src/profiles/management/commands/loader.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandParser
 from django.contrib.auth import get_user_model
-from cinegalaxy import utils
+from cinegalaxy import utils as cinegalaxy_utils
+
+from movies.models import Movie
 
 User = get_user_model()
 
@@ -8,19 +10,34 @@ class Command(BaseCommand):
     
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("count", nargs='?', default=10, type=int)
+        parser.add_argument("--movies", action="store_true", default=False)
+        parser.add_argument("--users", action="store_true", default=False)
         parser.add_argument("--show-total", action="store_true", default=False)
     
     def handle(self, *args, **options) -> str | None:
         count = options.get("count")
         show_total = options.get("show_total")
-        profiles = utils.get_fake_profiles(count=count)
+        load_movies = options.get("movies")
+        generate_users = options.get("users")
         
-        new_users = []
-        for profile in profiles:
-            new_users.append( User(**profile) )
-        
-        user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
-        print(f"New users: {len(user_bulk)}")
-        
-        if show_total:
-            print(f"Total users: {User.objects.count()}")
+        if load_movies:
+            movie_dataset = cinegalaxy_utils.load_movie_data(limit=count)
+            movies_new = [Movie(**mov) for mov in movie_dataset]
+            movie_bulk = Movie.objects.bulk_create(movies_new,
+                            ignore_conflicts=True)
+            print(f"New movies: {len(movie_bulk)}")
+            
+            if show_total:
+                print(f"Total movies: {Movie.objects.count()}")
+        elif generate_users:
+            profiles = cinegalaxy_utils.get_fake_profiles(count=count)
+            
+            new_users = []
+            for profile in profiles:
+                new_users.append( User(**profile) )
+            
+            user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
+            print(f"New users: {len(user_bulk)}")
+            
+            if show_total:
+                print(f"Total users: {User.objects.count()}")
