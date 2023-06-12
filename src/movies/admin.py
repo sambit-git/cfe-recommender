08edc2e6c7068d6d249152a996636a05b606ee23
Sambit Kumar <sambit.kumar142@gmail.com>
2023-06-12 18:35:40 +0530

    Updated Movies and Ratings Admin Displays Options

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Movie
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ["calculate_ratings_count",]
+    list_display = ["id", "title", "ratings_total", "rating_avg"]
     readonly_fields = [ "rating_avg_display" ]
     
 admin.site.register(Movie, MovieAdmin)
--- src/ratings/admin.py
+++ src/ratings/admin.py
@@ -5,6 +5,7 @@ from .models import Rating
 
 class RatingAdmin(admin.ModelAdmin):
     raw_id_fields = [ "user"]
+    list_display = [ "content_type", "content_object", "value" ]
     readonly_fields = ["content_object"]
 
 admin.site.register(Rating, RatingAdmin)
