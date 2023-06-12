2689f6f0fdf477d6899d7bdfd7a3a657777d7453
Sambit Kumar <sambit.kumar142@gmail.com>
2023-06-12 15:38:01 +0530

    Movie model - Calculate movie average rating
    - Calculate movie average rating
    - calculation can be performance intensive so calculate based on a time period
    - so added required fiels for ratings and last updated datetime

--- src/movies/admin.py
+++ src/movies/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Movie
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'calculate_ratings_count',]
-    readonly_fields = [ "calculate_ratings_count"]
+    list_display = ["calculate_ratings_count",]
+    readonly_fields = [ "rating_avg_display" ]
     
 admin.site.register(Movie, MovieAdmin)
--- src/movies/models.py
+++ src/movies/models.py
@@ -1,16 +1,51 @@
+from datetime import timedelta
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.utils import timezone
 
 from ratings.models import Rating
+
+RATING_UPDATE_PERIOD = 1
 
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=120, unique=True)
+    
     overview = models.TextField()
+    
     release_date =  models.DateTimeField(null=True, blank=True,
                         auto_now=False, auto_now_add=False)
+    
     timestamp = models.DateTimeField(auto_now_add=True)
+    
     ratings = GenericRelation(Rating)
+    
+    rating_updated = models.DateTimeField(null=True, blank=True,
+                        auto_now=False, auto_now_add=False)
+    
+    rating_avg = models.DecimalField(
+        max_digits=4, decimal_places=2, blank=True, null=True)
+    
+    ratings_total = models.IntegerField(null=True, blank=True)
+    
+    def rating_avg_display(self):
+        now = timezone.now()
+        if not self.rating_updated:
+            return self.calculate_rating()
+        if self.rating_updated > now - timedelta(minutes=RATING_UPDATE_PERIOD):
+            return self.rating_avg
+        return self.calculate_rating()
     
     def calculate_ratings_count(self):
         return self.ratings.all().count()
+    
+    def calculate_ratings_avg(self):
+        return self.ratings.all().avg()
+    
+    def calculate_rating(self, save=True):
+        self.ratings_total = self.calculate_ratings_count()
+        self.rating_avg = self.calculate_ratings_avg()
+        self.rating_updated = timezone.now()
+        if save:
+            self.save()
+        return self.rating_avg
