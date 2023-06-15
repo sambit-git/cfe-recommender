481cbff064829f59e528ac69cb71597edd161d6a
Sambit Kumar <sambit.kumar142@gmail.com>
2023-06-15 06:44:54 +0530

    Added moview views and templates

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Movie
+class MovieListView(ListView):
+    paginate_by = 100
+    queryset = Movie.objects.all().order_by("-rating_avg")
+
+class MovieDetailView(DetailView):
+    queryset = Movie.objects.all()
